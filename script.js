f2c200b4474ba3fbb89b0cc117622224b2daffa5
Noé Marguillard <noe.marguillard@essec.edu>
2019-02-25 22:54:37 +0100

    8 done

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function function_1() {
-  let footer = document.getElementsByTagName("footer")[0];
+  let footer = document.getElementsByTagName("footer")[0]
 
   footer.addEventListener("click", function(){
     console.log("Clique");
@@ -7,8 +7,8 @@ function function_1() {
 }
 
 function function_1_bis() {
-  let footer = document.getElementsByTagName("footer")[0];
-  let counter = 0;
+  let footer = document.getElementsByTagName("footer")[0]
+  let counter = 0
 
   footer.addEventListener("click", function(){
     console.log("Clique numéro " + ++counter);
@@ -16,8 +16,8 @@ function function_1_bis() {
 }
 
 function function_2() {
-  let button = document.querySelectorAll(".navbar-toggler")[0];
-  let header = document.getElementById("navbarHeader");
+  let button = document.querySelectorAll(".navbar-toggler")[0]
+  let header = document.getElementById("navbarHeader")
 
   button.addEventListener("click", function(){
     header.classList.contains('collapse') ? header.classList.remove("collapse") : header.classList.add("collapse");
@@ -25,11 +25,11 @@ function function_2() {
 }
 
 function function_3() {
-  let card_1 = document.querySelectorAll('.row .col-md-4')[0]
-  let button = document.getElementsByClassName("btn-group")[0].lastElementChild
+  let card_1 = document.querySelectorAll('.row .col-md-4')[0];
+  let button = document.getElementsByClassName("btn-group")[0].lastElementChild;
 
   button.addEventListener("click", function(){
-    card_1.style.color = "red"
+    card_1.style.color = "red";
   });
 }
 
@@ -42,9 +42,59 @@ function function_4() {
   });
 }
 
-//function_1();
-//function_1_bis();
-//function_2();
-//function_3();
-//function_4();
+function function_5() {
+  let navigation_bar = document.getElementsByClassName("navbar")[0]
+  let header = document.getElementsByTagName("header")[0]
+
+  header.addEventListener("dblclick", function(){
+    document.styleSheets[0].disabled == true ? document.styleSheets[0].disabled = false : document.styleSheets[0].disabled = true;
+  }); 
+}
+
+function function_6() {
+  let card = document.getElementsByClassName('col-md-4');
+  let buttons = document.getElementsByClassName('btn-group');
+
+  for (let i = 0; i < card.length; i++) {
+    buttons[i].firstElementChild.addEventListener('mouseover', function() {
+      if (card[i].getElementsByClassName('card-img-top')[0].style.width === "20%") {
+        card[i].getElementsByClassName('card-img-top')[0].style.width = "100%";
+        card[i].getElementsByClassName('card-text')[0].style.display = "block";
+      } else {
+        card[i].getElementsByClassName('card-img-top')[0].style.width = "20%";
+        card[i].getElementsByClassName('card-text')[0].style.display = "none";
+      }
+    });
+  }
+}
+
+function function_7() {
+  let card = document.getElementsByClassName('col-md-4');
+  let row = document.getElementsByClassName('row')[1];
+  let arrow = document.getElementsByClassName('btn-secondary')[0];
+
+  arrow.addEventListener('click', function() {
+    row.insertBefore(card[card.length-1], card[0]);
+  });
+}
+
+function function_8() {
+  let card = document.getElementsByClassName('col-md-4');
+  let row = document.getElementsByClassName('row')[1];
+  let arrow = document.getElementsByClassName("btn btn-primary my-2")[0];
+
+  arrow.addEventListener('click', function(event){
+    event.preventDefault();
+    row.appendChild(card[0]);
+  });
+}
+
+function_1();
+function_1_bis();
+function_2();
+function_3();
+function_4();
 function_5();
+function_6();
+function_7();
+function_8();
